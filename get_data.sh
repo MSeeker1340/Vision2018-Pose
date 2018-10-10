1621de4cf3abcd57b1f429e8dd243192d88a7833
Xingjian Guo <xg703@nyu.edu>
2018-10-10 17:42:04 -0400

    Silent unzipping

--- get_data.sh
+++ get_data.sh
@@ -12,10 +12,11 @@ wget http://msvocds.blob.core.windows.net/coco2014/val2014.zip
 wget http://msvocds.blob.core.windows.net/coco2014/test2014.zip
 wget http://msvocds.blob.core.windows.net/coco2015/test2015.zip
 
-unzip person_keypoints_trainval2014.zip -d dataset/COCO/
-unzip val2014.zip -d dataset/COCO/images
-unzip test2014.zip -d dataset/COCO/images
-unzip train2014.zip -d dataset/COCO/images
-unzip test2015.zip -d dataset/COCO/images
+echo "Unzipping files ..."
+unzip person_keypoints_trainval2014.zip -d dataset/COCO/ > /dev/null
+unzip val2014.zip -d dataset/COCO/images > /dev/null
+unzip test2014.zip -d dataset/COCO/images > /dev/null
+unzip train2014.zip -d dataset/COCO/images > /dev/null
+unzip test2015.zip -d dataset/COCO/images > /dev/null
 
 rm -f *.zip
